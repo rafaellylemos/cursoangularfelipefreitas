c0bbd88673ea102ecb367a6830e06c0fe71c1ef4
rafaellylemos <rafaellylemos@hotmail.com>
2024-02-27 16:38:01 -0300

    testing attributes

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,8 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss',
-  encapsulation: ViewEncapsulation.Emulated
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
 tipo:string = 'Simples';
